91d03fb77cdba4cb82aecb06c713d56e629c5861
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-12 00:05:47 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre
+   attr_accessor :name, :songs
+  @@all =[]
+   def initialize(name)
+     @name = name
+     @@all << self
+     @artists = []
+   end
+
+   def self.all
+     @@all
+   end
+
+   def songs
+     Song.all.select {|song| song.genre == self}
+   end
+
+   def artists
+     songs.map(&:artist)
+   end
+end
