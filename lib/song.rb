0984cc1f8cd22892a24f63c86590dedb949be2ce
Ryan Pethel <arpethel@gmail.com>
2019-04-13 23:40:09 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all.each {|song| puts song}
+  end
+end
